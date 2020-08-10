74a0bc36b4dcf5e4ba59c4de08b6cb48cb2d279c
Laiqa Mohid <homefolder@laiqas-mbp-2.home>
2020-08-10 17:08:10 +0100

    command shift p

---
+++ main.js
@@ -0,0 +1,100 @@
+let container = document.getElementById('container');
+let pressbtn = document.getElementById('pressbtn');
+let aboutbtn = document.getElementById('aboutbtn');
+let colbtn = document.getElementById('colbtn');
+let projbtn = document.getElementById('projbtn');
+
+let pressOpen = false;
+let aboutOpen = false;
+let colOpen = false;
+let projOpen = false;
+
+pressbtn.addEventListener('click', enablePress);
+aboutbtn.addEventListener('click', enableAbout);
+colbtn.addEventListener('click', enableCol);
+projbtn.addEventListener('click', enableProj);
+
+
+function enablePress(){
+    if (pressOpen === false) {
+        openPress();
+    } else {
+        closePress()
+    }
+}
+
+function enableAbout() {
+    if (aboutOpen === false) {
+        openAbout();
+    } else {
+        closeAbout();
+    }
+}
+
+function enableCol() {
+    if (colOpen === false) {
+        openCol();
+    } else {
+        closeCol();
+    }
+}
+
+function enableProj() {
+    if (projOpen === false) {
+        openProj();
+    } else {
+        closeProj();
+    }
+}
+
+function openPress() { 
+    container.classList.add('openPress')
+    pressOpen = true;
+}
+function closePress() { 
+ 
+    container.classList.remove('openPress')
+    pressOpen = false;
+}
+
+function openAbout() { 
+    container.classList.add('openAbout');
+    aboutOpen = true;
+}
+function closeAbout() { 
+   
+    container.classList.remove('openAbout');
+    aboutOpen = false;
+}
+
+function openCol() { 
+   
+    container.classList.add('openCollection');
+    colOpen = true;
+}
+
+function closeCol() {
+    container.classList.remove('openCollection')
+    colOpen = false;
+}
+
+function openProj() { 
+   
+    container.classList.add('openProjects');
+    projOpen = true;
+}
+
+function closeProj() {
+    container.classList.remove('openProjects')
+    projOpen = false;
+}
+
+const el = document.querySelector(".bg-home");
+
+el.addEventListener("mousemove", (e) => {
+  el.style.backgroundPositionX = -e.offsetX + "px";
+  el.style.backgroundPositionY = -e.offsetY + "px";
+});
+
+// make a [moving] grain img loop in p5
+// fire that
