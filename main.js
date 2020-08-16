59992cb67cd4189a82c3db57963e0489ffdd262f
lai.moh <laiqamohid73@gmail.com>
2020-08-16 23:43:10 +0100

    about-page

--- main.js
+++ main.js
@@ -15,86 +15,131 @@ colbtn.addEventListener('click', enableCol);
 projbtn.addEventListener('click', enableProj);
 
 
-function enablePress(){
-    if (pressOpen === false) {
-        openPress();
-    } else {
-        closePress()
-    }
+function enablePress() {
+   if (pressOpen === false) {
+      openPress();
+   } else {
+      closePress()
+   }
 }
 
 function enableAbout() {
-    if (aboutOpen === false) {
-        openAbout();
-    } else {
-        closeAbout();
-    }
+   if (aboutOpen === false) {
+      openAbout();
+   } else {
+      closeAbout();
+   }
 }
 
 function enableCol() {
-    if (colOpen === false) {
-        openCol();
-    } else {
-        closeCol();
-    }
+   if (colOpen === false) {
+      openCol();
+   } else {
+      closeCol();
+   }
 }
 
 function enableProj() {
-    if (projOpen === false) {
-        openProj();
-    } else {
-        closeProj();
-    }
+   if (projOpen === false) {
+      openProj();
+   } else {
+      closeProj();
+   }
 }
 
-function openPress() { 
-    container.classList.add('openPress')
-    pressOpen = true;
-}
-function closePress() { 
- 
-    container.classList.remove('openPress')
-    pressOpen = false;
+function openPress() {
+   container.classList.add('openPress')
+   pressOpen = true;
 }
 
-function openAbout() { 
-    container.classList.add('openAbout');
-    aboutOpen = true;
-}
-function closeAbout() { 
-   
-    container.classList.remove('openAbout');
-    aboutOpen = false;
+function closePress() {
+
+   container.classList.remove('openPress')
+   pressOpen = false;
 }
 
-function openCol() { 
-   
-    container.classList.add('openCollection');
-    colOpen = true;
+function openAbout() {
+   container.classList.add('openAbout');
+   aboutOpen = true;
+}
+
+function closeAbout() {
+
+   container.classList.remove('openAbout');
+   aboutOpen = false;
+}
+
+function openCol() {
+
+   container.classList.add('openCollection');
+   colOpen = true;
 }
 
 function closeCol() {
-    container.classList.remove('openCollection')
-    colOpen = false;
+   container.classList.remove('openCollection')
+   colOpen = false;
 }
 
-function openProj() { 
-   
-    container.classList.add('openProjects');
-    projOpen = true;
+function openProj() {
+
+   container.classList.add('openProjects');
+   projOpen = true;
 }
 
 function closeProj() {
-    container.classList.remove('openProjects')
-    projOpen = false;
+   container.classList.remove('openProjects')
+   projOpen = false;
 }
 
 const el = document.querySelector(".bg-home");
 
 el.addEventListener("mousemove", (e) => {
-  el.style.backgroundPositionX = -e.offsetX + "px";
-  el.style.backgroundPositionY = -e.offsetY + "px";
+   el.style.backgroundPositionX = -e.offsetX + "px";
+   el.style.backgroundPositionY = -e.offsetY + "px";
 });
 
-// make a [moving] grain img loop in p5
-// fire that
+
+//  language button toggle
+
+let engBox = document.getElementById('eng'),
+   manBox = document.getElementById('man')
+langbtn = document.querySelector('.langChange');
+
+langbtn.addEventListener('click', function () {
+   console.log(engBox.classList);
+   if (engBox.classList.contains('hidden')) {
+      engBox.classList.remove('hidden');
+      setTimeout(function () {
+         engBox.classList.remove('visuallyhidden');
+      }, 20);
+   } else {
+      engBox.classList.add('visuallyhidden');
+      engBox.addEventListener('transitionend', function (e) {
+         engBox.classList.add('hidden');
+      }, {
+         capture: false,
+         once: true,
+         passive: false
+      });
+   }
+
+}, false);
+
+langbtn.addEventListener('click', function () {
+   console.log(manBox.classList);
+   if (manBox.classList.contains('seen')) {
+      manBox.classList.remove('seen');
+      setTimeout(function () {
+         manBox.classList.remove('visual');
+      }, 20);
+   } else {
+      manBox.classList.add('viusal');
+      manBox.addEventListener('transitionend', function (e) {
+         manBox.classList.add('seen');
+      }, {
+         capture: false,
+         once: true,
+         passive: false
+      });
+   }
+}, false);
