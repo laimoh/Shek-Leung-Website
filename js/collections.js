9dce3e3e66ae26f3763d3d8012f6c9d350a5242e
lai.moh <laiqamohid73@gmail.com>
2020-09-03 18:22:03 +0100

    collection final touch up - intersection observer details

--- js/collections.js
+++ js/collections.js
@@ -1,8 +1,3 @@
-// 
-
-
-// collections page tabs toggle
-
 const tabs = document.querySelectorAll('[data-tab-target]');
 const links = document.getElementsByClassName('tab');
 const tabContents = document.querySelectorAll('[data-tab-content]')
@@ -10,23 +5,11 @@ const audiobtn = document.getElementById('audio');
 const playbtn = document.getElementById('pause');
 const vid = document.getElementById("catwalk");
 
-
-// loop through the list to find the one tab mouse clicked
-tabs.forEach(tab => {
-   tab.addEventListener('click', () => {
-      const target = document.querySelector(tab.dataset.tabTarget)
-
-      // tabContents.forEach(tabContent => {
-      //    tabContent.classList.remove('active')
-      // })
-      tabs.forEach(tab => {
-         tab.classList.remove('active')
-      });
-      tab.classList.add('active')
-   });
-
-
-});
+const show = document.getElementById('show');
+const about = document.getElementById('about');
+const  researchFilm = document.getElementById('researchFilm');
+const stills = document.getElementById('stills');
+const lookbook = document.getElementById('lookbook');
 
 // content fade in
 
@@ -35,19 +18,21 @@ const faders = document.querySelectorAll('.fade-in');
 
 const colorOptions = {
    root: null, // it is the viewport
-   threshold: 0, // between zero and one  - how much of the elemtn should in viewport for object to fire an event(1 is 100%)
-   rootMargin: "-150px" // margin between firing
+   threshold: 0.3,
+   rootMargin: "0px 0px 0px -150px"
 };
 const colorObserver = new IntersectionObserver(function (entries, colorObserver) {
    entries.forEach(entry => {
       if (!entry.isIntersecting) {
          document.querySelectorAll(".nav-bar a").forEach(e => e.style.color = "var(--grCol3");
          document.querySelectorAll(".info a").forEach(e => e.style.color = "var(--grCol3");
+         document.querySelector('.user-border').style.borderColor = "white";
          audiobtn.style.display = "block";
       } else {
          document.querySelectorAll(".nav-bar a").forEach(e => e.style.color = "black");
          document.querySelector(".active").style.color = "var(--grCol3)";
          document.querySelectorAll(".info a").forEach(e => e.style.color = "black");
+         document.querySelector('.user-border').style.borderColor = "black";
       }
    })
 }, colorOptions);
@@ -59,7 +44,7 @@ colorElements.forEach(element => {
 
 
 const appearOptions = {
-   threshold: 0.8,
+   threshold: 0.3,
    rootMargin: "0px 0px 0px -150px"
 };
 
@@ -73,10 +58,46 @@ const appearOnScroll = new IntersectionObserver(function (entries, appearOnScrol
       }
    })
 }, appearOptions)
+
 faders.forEach(fader => {
    appearOnScroll.observe(fader);
 })
 
+const beActiveOptions = {
+   threshold: 0.3,
+   rootMargin: "0px 0px 0px -150px"
+};
+
+
+const beActive = new IntersectionObserver(function (entries, beActive) {
+   entries.forEach(entry => {
+      if (entry.target.id === 'about' && entry.isIntersecting) {
+         tabs.forEach(tab => {
+            tab.classList.remove('active')});
+         tabs[1].classList.add('active');
+      } else if (entry.target.id === 'researchFilm' && entry.isIntersecting) {
+         tabs.forEach(tab => {
+            tab.classList.remove('active')});
+         tabs[2].classList.add('active');
+      } else if (entry.target.id === 'stills' && entry.isIntersecting) {
+         tabs.forEach(tab => {
+            tab.classList.remove('active')});
+         tabs[3].classList.add('active');
+      } else if (entry.target.id === 'lookbook' && entry.isIntersecting) {
+         tabs.forEach(tab => {
+            tab.classList.remove('active')});
+         tabs[4].classList.add('active');
+      } else if (entry.target.id === 'show' && entry.isIntersecting) {
+         tabs.forEach(tab => {
+            tab.classList.remove('active')});
+         tabs[0].classList.add('active');
+      }
+   })
+}, beActiveOptions)
+
+tabContents.forEach(tabContent => {
+   beActive.observe(tabContent);
+})
 
 // video control
 if (!audiobtn) {
@@ -146,12 +167,3 @@ slider.addEventListener('transitionend', () => {
       slider.style.transform = 'translateX(' + (-size * counter) + 'px)'
    }
 })
-
-function getURL() {
-   let w = (window.location.href);
-   if (w.includes('MA.html')) {
-      
-   };
-}
-
-getURL()
